7f89cd0356fc1a250133d4e627b559226521f57d
panda725 <81299585+panda725@users.noreply.github.com>
2024-01-05 10:34:33 +0000

    Deploying to gh-pages from @ panda725/template@3bc6862a2fce263bd5795f23af1b97393528373d 🚀

---
+++ index.3fa09018.js
@@ -0,0 +1,2 @@
+!function(){for(var o=0;o<10;o+=1)console.log("".concat(10," % ").concat(o," = "),10%o)}();
+//# sourceMappingURL=index.3fa09018.js.map
---
+++ index.9174663e.js
@@ -0,0 +1,2 @@
+for(let o=0;o<10;o+=1)console.log(`10 % ${o} = `,10%o);
+//# sourceMappingURL=index.9174663e.js.map
